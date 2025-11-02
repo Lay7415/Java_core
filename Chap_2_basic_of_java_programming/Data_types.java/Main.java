1f282d395bea0d84b090eb5491945dd7bc3c02fb
Lay7415 <ieye7531@gmail.com>
2025-11-02 16:35:16 +0600

    feat(data_types): complete the theory on data types

---
+++ Chap_2_basic_of_java_programming/Data_types.java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(Strings[] args) {
+        boolean bool_var_a = true;
+        boolean bool_var_b = false;
+
+        byte byte_var = 1;
+        short short_var = 2;
+        int int_var = 4;
+        long long_var = 8;
+        double double_var = 8.00f;
+        float float_var = 4.23;
+        char char_var = '2'
+    }
+}
