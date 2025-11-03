7649b7da1f13e6c53aeb4e5ce242eeb32b19b0d8
Lay7415 <ieye7531@gmail.com>
2025-11-03 11:25:09 +0600

    feat(Chapter2): learn arrays and matrix

--- Chap_2_basic_of_java_programming/Arrays.java
+++ Chap_2_basic_of_java_programming/Arrays.java
@@ -1,6 +1,22 @@
 package Chap_2_basic_of_java_programming;
 
 public class Arrays {
+    public static void main(String[] args) {
+        int[][] matrix = new int[10][10];
 
-    
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = i * j;
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                int element = matrix[i][j];
+                System.out.printf("%d\t", element);
+            }
+            System.out.println();
+            System.out.println();
+        }
+    }
 }
