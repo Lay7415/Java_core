b7aaaa3e24f9cc77986163ec5b3245648f69eb84
Lay7415 <ieye7531@gmail.com>
2025-11-03 11:00:49 +0600

    feat(Chapter_2): learn primitive type conversion

---
+++ Chap_2_basic_of_java_programming/Primitive_Type_Conversion.java
@@ -0,0 +1,10 @@
+package Chap_2_basic_of_java_programming;
+
+public class Primitive_Type_Conversion {
+
+    public static void main(String[] args) {
+        double a = 56.9898;
+        int b = (int) Math.round(a);
+        System.out.println(b); 
+    }
+}
